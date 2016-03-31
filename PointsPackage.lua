b2d984333f63d9a3bc381a932f331b1604594599
WH <ACOKing@users.noreply.github.com>
2016-03-31 10:11:37 -0800

    Added Constructor

--- PointsPackage.lua
+++ PointsPackage.lua
@@ -1,3 +1,10 @@
 -- PointsPackage object for Shared Player Points
 
 local PointsPackage = {}
+
+
+--[[ creates the shared player points object for use to reward players after defeating enemies
+]]--
+function PointsPackage.new(points,players)
+
+end
