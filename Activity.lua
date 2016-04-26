5aa350d752ed445b4d47c6a38652ad2ac2ff6dee
WH <ACOKing@users.noreply.github.com>
2016-04-26 15:55:53 -0400

    Update Activity.lua

--- Activity.lua
+++ Activity.lua
@@ -1,1 +1,7 @@
 local Activity = {}
+function Activity:Create(dat,initialMessage,act1,act2,act3,success,err)
+
+game.ReplicatedStorage.RemoteService.ShowDialog:
+FireClient(dat.Player,dat.Player.Name,initialMessage, 10)
+
+end
